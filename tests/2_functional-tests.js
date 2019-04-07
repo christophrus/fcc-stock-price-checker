82f5da750f065008b888466d61f3e10b9cc5e64e
christophrus <christophrus@protonmail.com>
2019-04-08 01:50:04 +0200

    added chai.js tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -14,6 +14,8 @@ var server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
+
+    var testLikes;
     
     suite('GET /api/stock-prices => stockData object', function() {
       
@@ -21,29 +23,91 @@ suite('Functional Tests', function() {
        chai.request(server)
         .get('/api/stock-prices')
         .query({stock: 'goog'})
-        .end(function(err, res){
-          
-          //complete this one too
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.isDefined(res.body.stockData);
+          assert.property(res.body.stockData, "stock");
+          assert.property(res.body.stockData, "price");
+          assert.property(res.body.stockData, "likes");
+          assert.equal(res.body.stockData.stock, 'GOOG');
           
           done();
         });
       });
       
       test('1 stock with like', function(done) {
-        
+        chai.request(server)
+        .get('/api/stock-prices')
+        .query({stock: 'msft', like: 'true'})
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.isDefined(res.body.stockData);
+          assert.property(res.body.stockData, "stock");
+          assert.property(res.body.stockData, "price");
+          assert.property(res.body.stockData, "likes");
+          assert.equal(res.body.stockData.stock, 'MSFT');
+          assert.isAbove(res.body.stockData.likes, 0)
+          testLikes = res.body.stockData.likes;
+          done();
+        });    
       });
-      
+
       test('1 stock with like again (ensure likes arent double counted)', function(done) {
-        
+        chai.request(server)
+        .get('/api/stock-prices')
+        .query({stock: 'msft', like: 'true'})
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.isDefined(res.body.stockData);
+          assert.property(res.body.stockData, "stock");
+          assert.property(res.body.stockData, "price");
+          assert.property(res.body.stockData, "likes");
+          assert.equal(res.body.stockData.stock, 'MSFT');
+          assert.equal(res.body.stockData.likes, testLikes);
+          done();
+        });
       });
-      
+
       test('2 stocks', function(done) {
-        
+        chai.request(server)
+        .get('/api/stock-prices')
+        .query({stock: ['msft','goog']})
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.isArray(res.body.stockData);
+          assert.property(res.body.stockData[0], "stock");
+          assert.property(res.body.stockData[0], "price");
+          assert.property(res.body.stockData[0], "rel_likes");
+          assert.property(res.body.stockData[1], "stock");
+          assert.property(res.body.stockData[1], "price");
+          assert.property(res.body.stockData[1], "rel_likes");
+          assert.oneOf(res.body.stockData[0].stock, ['GOOG', 'MSFT']);
+          assert.oneOf(res.body.stockData[1].stock, ['GOOG', 'MSFT']);
+          assert.equal(res.body.stockData[0].rel_likes + res.body.stockData[1].rel_likes, 0);
+          done();
+        });
+      });
+
+      test('2 stocks with like', function(done) {
+        chai.request(server)
+        .get('/api/stock-prices')
+        .query({stock: ['msft','goog'], likes: 'true'})
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.isArray(res.body.stockData);
+          assert.property(res.body.stockData[0], "stock");
+          assert.property(res.body.stockData[0], "price");
+          assert.property(res.body.stockData[0], "rel_likes");
+          assert.property(res.body.stockData[1], "stock");
+          assert.property(res.body.stockData[1], "price");
+          assert.property(res.body.stockData[1], "rel_likes");
+          assert.oneOf(res.body.stockData[0].stock, ['GOOG', 'MSFT']);
+          assert.oneOf(res.body.stockData[1].stock, ['GOOG', 'MSFT']);
+          assert.equal(res.body.stockData[0].rel_likes + res.body.stockData[1].rel_likes, 0);
+          done();
+        });
       });
       
-      test('2 stocks with like', function(done) {
-        
-      });
       
     });
 
