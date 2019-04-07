2154a7be88cb04f1072f3b571d01b025c5d781b8
christophrus <christophrus@protonmail.com>
2019-04-08 01:49:35 +0200

    added helmet.js and custom error handling in server.js

--- server.js
+++ server.js
@@ -4,12 +4,23 @@ var express     = require('express');
 var bodyParser  = require('body-parser');
 var expect      = require('chai').expect;
 var cors        = require('cors');
+var helmet      = require('helmet');
 
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
 var runner            = require('./test-runner');
 
 var app = express();
+
+app.use(helmet({
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ["'self'"]
+    }
+  },
+  noCache: true
+}));
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -37,9 +48,27 @@ app.use(function(req, res, next) {
     .send('Not Found');
 });
 
+// Error Handling middleware
+app.use((err, req, res, next) => {
+  let errCode, errMessage;
+
+  if (err.errors) {
+    // mongoose validation error
+    errCode = 400; // bad request
+    const keys = Object.keys(err.errors);
+    // report the first validation error
+    errMessage = err.errors[keys[0]].message;
+  } else {
+    // generic or custom error
+    errCode = err.status || 500;
+    errMessage = err.message || 'Internal Server Error';
+  }
+  res.status(errCode).send(errMessage);
+})
+
 //Start our server and tests!
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Listening on port " + process.env.PORT);
+const listener = app.listen(process.env.PORT || 3000, function () {
+  console.log("Listening on port " + listener.address().port);
   if(process.env.NODE_ENV==='test') {
     console.log('Running Tests...');
     setTimeout(function () {
